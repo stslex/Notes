20ebdf77466f8687813979e97657ce495a9837c1
slex <ilya977.077@gmail.com>
2021-08-19 21:17:24 +0300

    add time/datestamp. Code refactor

--- app/src/main/java/com/stslex93/notes/ui/edit/EditFragment.kt
+++ app/src/main/java/com/stslex93/notes/ui/edit/EditFragment.kt
@@ -15,6 +15,7 @@ import com.stslex93.notes.databinding.FragmentEditBinding
 import com.stslex93.notes.ui.NoteViewModel
 import com.stslex93.notes.utilites.BaseFragment
 import com.stslex93.notes.utilites.hideKeyBoard
+import com.stslex93.notes.utilites.observeOnce
 import java.text.SimpleDateFormat
 
 class EditFragment : BaseFragment() {
@@ -24,6 +25,8 @@ class EditFragment : BaseFragment() {
 
     private var flagEdit = false
     private lateinit var id: String
+    private lateinit var editTitle: String
+    private lateinit var editContent: String
 
     private val viewModel: NoteViewModel by viewModels { viewModelFactory.get() }
 
@@ -52,13 +55,30 @@ class EditFragment : BaseFragment() {
         val args: EditFragmentArgs by navArgs()
         id = args.id
         flagEdit = args.edit
+
+        val datestamp =
+            SimpleDateFormat(getString(R.string.date_format)).format(System.currentTimeMillis())
         if (flagEdit) {
             binding.editCardView.transitionName = id
-            viewModel.note(id).observe(viewLifecycleOwner) { note ->
-                binding.editInputTitle.editText?.setText(note.title)
-                binding.editInputContent.editText?.setText(note.content)
+            viewModel.note(id).observeOnce(viewLifecycleOwner) { note ->
+                editTitle = note.title
+                editContent = note.content
+                binding.editInputTitle.editText?.setText(editTitle)
+                binding.editInputContent.editText?.setText(editContent)
+                if (datestamp == note.datestamp) {
+                    binding.editTime.text = "${getString(R.string.label_edit)}  ${note.timestamp}"
+                } else {
+                    binding.editTime.text =
+                        "${getString(R.string.label_edit)} ${note.datestamp}"
+                }
+
             }
-        } else binding.editCardView.transitionName = getString(R.string.default_transition_name)
+        } else {
+            binding.editCardView.transitionName = getString(R.string.default_transition_name)
+            binding.editTime.text = "${getString(R.string.label_edit)}  ${
+                SimpleDateFormat(getString(R.string.time_format)).format(System.currentTimeMillis())
+            }"
+        }
 
     }
 
@@ -70,24 +90,23 @@ class EditFragment : BaseFragment() {
     private fun saveData() {
         val title = binding.editInputTitle.editText?.text.toString()
         val content = binding.editInputContent.editText?.text.toString()
-
         val datestamp =
             SimpleDateFormat(getString(R.string.date_format)).format(System.currentTimeMillis())
         val timestamp =
             SimpleDateFormat(getString(R.string.time_format)).format(System.currentTimeMillis())
+        val note = Note(
+            id.toInt(),
+            title = title,
+            content = content,
+            datestamp = datestamp,
+            timestamp = timestamp
+        )
 
-        if (flagEdit) {
-            val note = Note(id.toInt(), title, content, datestamp, timestamp)
+        if (flagEdit
+            && (editTitle != title || editContent != content)
+        ) {
             viewModel.update(note)
         } else if (title.isNotEmpty() || content.isNotEmpty()) {
-            val note =
-                Note(
-                    id.toInt(),
-                    title = title,
-                    content = content,
-                    datestamp = datestamp,
-                    timestamp = timestamp
-                )
             viewModel.insert(note)
         }
 
