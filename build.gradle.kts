81566bdce3f44c4d95eb0ae5b72923b939efaced
stslex <ilya977.077@gmail.com>
2022-04-24 12:35:57 +0300

    add kotlin symbol processing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 plugins {
     id("com.android.application") version "7.0.2" apply false
     id("com.android.library") version "7.0.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.6.20" apply false
-    kotlin("jvm") version "1.6.20"
-    kotlin("plugin.serialization") version "1.6.20"
+    id("org.jetbrains.kotlin.android") version "1.6.21" apply false
+    kotlin("jvm") version "1.6.21" apply false
+    kotlin("plugin.serialization") version "1.6.21"
 }
 
 buildscript {
     dependencies {
+        classpath(kotlin("gradle-plugin", version = "1.6.21"))
         classpath("com.android.tools.build:gradle:7.1.3")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.2")
     }
 }
