73c5ca103f9c5c504cd5538f666409b06a39d551
stslex <ilya977.077@gmail.com>
2023-08-22 19:09:44 +0300

    fix click on content

--- app/src/main/java/com/stslex93/notes/ui/widget/ComposeCustomTextView.kt
+++ app/src/main/java/com/stslex93/notes/ui/widget/ComposeCustomTextView.kt
@@ -2,17 +2,11 @@ package com.stslex93.notes.ui.widget
 
 import android.content.Context
 import android.util.AttributeSet
-import androidx.compose.animation.core.LinearOutSlowInEasing
-import androidx.compose.animation.core.animateIntAsState
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.clickable
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 
@@ -23,26 +17,14 @@ class ComposeCustomTextView @JvmOverloads constructor(
 ) : BaseComposeCustomWidget(context, attrs, defStyleAttr) {
 
     private val textState = mutableStateOf(String())
+    private var onClick: () -> Unit = {}
 
     @Composable
     override fun MainContent(modifier: Modifier) {
-        var isExpand by remember {
-            mutableStateOf(false)
-        }
-        val maxLines by animateIntAsState(
-            targetValue = if (isExpand) 20 else 2,
-            animationSpec = tween(
-                durationMillis = 300,
-                delayMillis = 50,
-                easing = LinearOutSlowInEasing
-            )
-        )
         Text(
-            modifier = modifier.clickable {
-                isExpand = isExpand.not()
-            },
+            modifier = modifier.clickable(onClick = onClick),
             text = textState.value,
-            maxLines = maxLines,
+            maxLines = 2,
             overflow = TextOverflow.Ellipsis,
             style = MaterialTheme.typography.bodyMedium,
             color = MaterialTheme.colorScheme.onSurface
@@ -51,5 +33,11 @@ class ComposeCustomTextView @JvmOverloads constructor(
 
     fun setText(text: String) {
         textState.value = text
+    }
+
+    fun setClickListener(
+        onClick: () -> Unit
+    ) {
+        this.onClick = onClick
     }
 }
