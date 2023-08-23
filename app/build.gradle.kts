74fa9bc5b089747fcd9a791666fb5771ba6074f8
Ilya Stepanyuk <ilya977.077@gmail.com>
2023-08-23 22:23:31 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,39 +9,6 @@ plugins {
 
 android.namespace = "com.stslex93.notes"
 
-android {
-    signingConfigs {
-        val keystoreProperties = gradleKeystoreProperties(project.rootProject.projectDir)
-        create("release") {
-            keyAlias = keystoreProperties.getProperty("keyAlias")
-            keyPassword = keystoreProperties.getProperty("keyPassword")
-            storeFile = getFile(keystoreProperties.getProperty("storeFile"))
-            storePassword = keystoreProperties.getProperty("storePassword")
-        }
-        with(get("debug")) {
-            keyAlias = keystoreProperties.getProperty("keyAlias")
-            keyPassword = keystoreProperties.getProperty("keyPassword")
-            storeFile = getFile(keystoreProperties.getProperty("storeFile"))
-            storePassword = keystoreProperties.getProperty("storePassword")
-        }
-    }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-            signingConfig = signingConfigs.getByName("release")
-            isDebuggable = false
-        }
-        getByName("debug") {
-            signingConfig = signingConfigs.getByName("debug")
-            isDebuggable = true
-        }
-    }
-}
-
 dependencies {
     implementation(project(":core:ui"))
     implementation(project(":core:database"))
@@ -49,25 +16,4 @@ dependencies {
     implementation(project(":core:navigation"))
     implementation(project(":feature:home"))
     implementation(project(":feature:edit"))
-}
-
-fun getFile(path: String): File {
-    val file = File(project.rootProject.projectDir, path)
-    if (file.isFile) {
-        return file
-    } else {
-        throw IllegalStateException("${file.name} is inValid")
-    }
-}
-
-fun gradleKeystoreProperties(projectRootDir: File): Properties {
-    val properties = Properties()
-    val localProperties = File(projectRootDir, "keystore.properties")
-
-    if (localProperties.isFile) {
-        InputStreamReader(FileInputStream(localProperties), Charsets.UTF_8).use { reader ->
-            properties.load(reader)
-        }
-    }
-    return properties
 }
