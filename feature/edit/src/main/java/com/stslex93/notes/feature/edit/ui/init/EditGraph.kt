5c64653f00202ceb169c0e85d92e50421200226f
stslex <ilya977.077@gmail.com>
2023-09-06 23:14:43 +0300

    init

--- feature/edit/src/main/java/com/stslex93/notes/feature/edit/ui/init/EditGraph.kt
+++ feature/edit/src/main/java/com/stslex93/notes/feature/edit/ui/init/EditGraph.kt
@@ -3,24 +3,22 @@ package com.stslex93.notes.feature.edit.ui.init
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraphBuilder
-import com.stslex93.notes.core.navigation.model.AppDestination
-import com.stslex93.notes.core.navigation.utils.NavExt.composableArguments
 import com.stslex93.notes.core.navigation.v2.NoteEditArgs
-import com.stslex93.notes.core.navigation.v2.compose.composableArg
+import com.stslex93.notes.core.navigation.v2.compose.composable
 import com.stslex93.notes.core.navigation.v2.screen.HostScreen
 import com.stslex93.notes.feature.edit.di.setupComponent
 
 fun NavGraphBuilder.editGraph(
     modifier: Modifier = Modifier,
 ) {
-    composableArg<NoteEditArgs>(HostScreen.NOTE_EDIT) { noteEditArgs ->
-        val arguments = noteEditArgs ?: NoteEditArgs(-1, false)
-
-        val viewModel = setupComponent(arguments.hashCode().toString())
-
-        LaunchedEffect(Unit) {
-            viewModel.init(arguments)
-        }
+    composable(HostScreen.NOTE_EDIT.route) { _, _ ->
+//        val arguments = noteEditArgs ?: NoteEditArgs(-1, false)
+//
+        val viewModel = setupComponent("")
+//
+//        LaunchedEffect(Unit) {
+//            viewModel.init(arguments)
+//        }
 
         InitEditScreen(
             modifier = modifier,
